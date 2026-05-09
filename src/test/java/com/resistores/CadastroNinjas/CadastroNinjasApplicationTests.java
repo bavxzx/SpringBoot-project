645c2fa6e39f9c8005903682613f9019b85d6600
bavxzx <fabioevangelistaferrazolli@gmail.com>
2026-05-09 19:31:10 -0300

    Initial commit

---
+++ src/test/java/com/resistores/CadastroNinjas/CadastroNinjasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resistores.CadastroNinjas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroNinjasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
