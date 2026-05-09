bf16d2fbacff8dbdcc5cb9bb7efba093b5ab1f88
bavxzx <fabioevangelistaferrazolli@gmail.com>
2026-05-09 19:45:30 -0300

    Criação de controller

---
+++ src/main/java/com/resistores/CadastroNinjas/Controller/Controller.java
@@ -0,0 +1,15 @@
+package com.resistores.CadastroNinjas.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/helloWorld")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
